1a52e5b49fa55c6ed4da613a1965b5518fdf8998
nguyenvantra <dkdarknessnvt@gmail.com>
2017-11-26 20:57:41 +0700

    Add Custom Fonts

---
+++ app/resources/Fonts.js
@@ -0,0 +1,14 @@
+/**
+ * @author nguyenvantra
+ * @email dkdarknessnvt@gmail.com
+ * @create date 2017-11-26 06:16:54
+ * @modify date 2017-11-26 06:16:54
+ * @version 1.0.0
+ * @since 1.0.0
+ * @desc Custom font for application
+*/
+
+export const Fonts = {
+  FontRegular: 'Raleway-Regular',
+  FontLight: 'Raleway-ExtraLight',
+}
