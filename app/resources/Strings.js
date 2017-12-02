f21637619089867751136dc27949ac6183287902
nguyenvantra <dkdarknessnvt@gmail.com>
2017-12-02 22:40:57 +0700

    add Strings file resources

---
+++ app/resources/Strings.js
@@ -0,0 +1,13 @@
+/**
+ * @author nguyenvantra
+ * @email dkdarknessnvt@gmail.com
+ * @create date 2017-12-02 10:37:13
+ * @modify date 2017-12-02 10:37:13
+ * @version 1.0.0
+ * @since 1.0.0
+ * @desc [description]
+*/
+
+export default Strings = {
+  contentTextAnim: 'Animate'
+}
