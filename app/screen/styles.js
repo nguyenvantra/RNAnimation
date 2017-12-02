d16377b2e58c831d3d18755bce23dc3ccbbb5d15
nguyenvantra <dkdarknessnvt@gmail.com>
2017-12-02 22:41:57 +0700

    add file styles of screen

---
+++ app/screen/styles.js
@@ -0,0 +1,17 @@
+/**
+ * @author nguyenvantra
+ * @email dkdarknessnvt@gmail.com
+ * @create date 2017-12-02 10:27:28
+ * @modify date 2017-12-02 10:27:28
+ * @version 1.0.0
+ * @since 1.0.0
+ * @desc Customize style of screen
+*/
+
+import { StyleSheet } from 'react-native';
+
+export default styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
