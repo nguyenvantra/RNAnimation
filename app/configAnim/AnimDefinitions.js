b98522d43a544fa3789a8fb9cfa5244e7204b014
nguyenvantra <dkdarknessnvt@gmail.com>
2017-12-05 23:28:26 +0700

    Add config Animation

---
+++ app/configAnim/AnimDefinitions.js
@@ -0,0 +1,12 @@
+/**
+ * @author nguyenvantra
+ * @email dkdarknessnvt@gmail.com
+ * @create date 2017-12-04 12:05:53
+ * @modify date 2017-12-04 12:05:53
+ * @version 1.0.0
+ * @since 1.0.0
+ * @desc Definitions anim
+*/
+
+//Attention Seekers
+
---
+++ app/configAnim/AnimTypes.js
@@ -0,0 +1,21 @@
+/**
+ * @author nguyenvantra
+ * @email dkdarknessnvt@gmail.com
+ * @create date 2017-12-03 08:37:55
+ * @modify date 2017-12-03 08:37:55
+ * @version 1.0.0
+ * @since 1.0.0
+ * @desc Animation Types
+*/
+export const AttentionSeekersAnimTypes = [
+  {value: 'bounce'},
+  {value: 'flash'},
+  {value: 'jello'},
+  {value: 'pulse'},
+  {value: 'rotate'},
+  {value: 'shake'},
+  {value: 'swing'},
+  {value: 'rubberBand'},
+  {value: 'tada'},
+  {value: 'wobble'},
+]
